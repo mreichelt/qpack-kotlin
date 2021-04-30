bbf2ecc2c771e48116370e27422b479aa367f4ae
Marc Reichelt <mcreichelt@gmail.com>
2021-04-30 11:35:18 +0200

    #2 Add QPACK static table from RFC

--- src/commonTest/kotlin/de/marcreichelt/qpack/QpackTest.kt
+++ src/commonTest/kotlin/de/marcreichelt/qpack/QpackTest.kt
@@ -1,5 +1,23 @@
 package de.marcreichelt.qpack
 
+import de.marcreichelt.qpack.Qpack.staticTable
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
 class QpackTest {
+
+    @Test
+    fun checkStaticTable() {
+        assertEquals(staticTable.size, 99) // qpack v21
+
+        assertEquals(Header(":authority", ""), staticTable[0])
+        assertEquals(Header(":status", "404"), staticTable[27])
+        assertEquals(
+            Header("content-security-policy", "script-src 'none'; object-src 'none'; base-uri 'none'"),
+            staticTable[85]
+        )
+        assertEquals(Header("x-frame-options", "sameorigin"), staticTable[98])
+    }
 
 }
