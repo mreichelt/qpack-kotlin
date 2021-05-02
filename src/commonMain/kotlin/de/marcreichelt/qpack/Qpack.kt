6585f9782a6f923880735573df1f664cdccdce9b
Marc Reichelt <mcreichelt@gmail.com>
2021-05-02 16:07:12 +0200

    fix #5 encode prefixed integers
    - also: add reference encoder implementation (draft, not tested yet)

--- src/commonMain/kotlin/de/marcreichelt/qpack/Qpack.kt
+++ src/commonMain/kotlin/de/marcreichelt/qpack/Qpack.kt
@@ -6,8 +6,9 @@ import de.marcreichelt.qpack.Header.Companion.TARGET_AUTHORITY
 import de.marcreichelt.qpack.Header.Companion.TARGET_METHOD
 import de.marcreichelt.qpack.Header.Companion.TARGET_PATH
 import de.marcreichelt.qpack.Header.Companion.TARGET_SCHEME
+import okio.Buffer
 import okio.ByteString
-import okio.ByteString.Companion.decodeHex
+import kotlin.math.max
 
 inline class QuicStreamId(val value: Int) {
     init {
@@ -20,7 +21,7 @@ object Qpack {
     /**
      * Static table as defined in [QPACK](https://quicwg.org/base-drafts/draft-ietf-quic-qpack.html#section-appendix.a)
      */
-    val staticTable = listOf(
+    val staticTable: List<Header> = listOf(
         Header(TARGET_AUTHORITY, ""),
         Header(TARGET_PATH, "/"),
         Header("age", "0"),
@@ -122,18 +123,162 @@ object Qpack {
         Header("x-frame-options", "sameorigin"),
     )
 
+    val dynamicTable: MutableList<Header> = mutableListOf()
+
     class Writer(
-        val maxDynamicTableCapacity: Int = 0,
+        val out: Buffer,
+        val encoderBuffer: Buffer,
+        private val maxDynamicTableCapacity: Int = 0,
         val maxBlockedStreams: Int = 0,
         val acknowledgementMode: AcknowledgementMode = None,
     ) {
 
-        fun encode(streamId: QuicStreamId, headers: List<Header>): ByteString {
-            return "0000".decodeHex()
+        fun encode(headers: List<Header>): ByteString {
+            val base = dynamicTable.size
+            var requiredInsertCount = 0
+
+            headers.forEach { header ->
+                val staticIndex = findInStaticTable(header)
+                if (staticIndex != -1) {
+                    encodeStaticIndexReference(out, staticIndex)
+                    return@forEach
+                }
+
+                var dynamicIndex = findInDynamicTable(header)
+                var staticNameIndex = -1
+                var dynamicNameIndex = -1
+                if (dynamicIndex == -1) {
+                    // No matching entry.  Either insert+index or encode literal
+                    staticNameIndex = findNameInStaticTable(header.name)
+                    if (staticNameIndex == -1) {
+                        dynamicNameIndex = findNameInDynamicTable(header.name)
+                    }
+
+                    if (shouldIndex(header) and dynamicTableCanIndex(header)) {
+                        encodeInsert(staticNameIndex, dynamicNameIndex, header)
+                        dynamicTable += header
+                        dynamicIndex = dynamicTable.lastIndex
+                    }
+                }
+
+                if (dynamicIndex == -1) {
+                    // Could not index it, literal
+                    if (dynamicNameIndex != -1) {
+                        // Encode literal with dynamic name, possibly above base
+                        encodeDynamicLiteral(dynamicNameIndex, base, header)
+                        requiredInsertCount = max(requiredInsertCount, dynamicNameIndex)
+                    } else {
+                        // Encodes a literal with a static name or literal name
+                        encodeLiteral(staticNameIndex, header)
+                    }
+                } else {
+                    // Dynamic index reference
+                    require(dynamicIndex != -1)
+                    requiredInsertCount = max(requiredInsertCount, dynamicIndex)
+                    // Encode dynamicIndex, possibly above base
+                    encodeDynamicIndexReference(dynamicIndex, base)
+                }
+
+            }
+
+            // encode the prefix
+            val prefixBuffer = Buffer()
+            if (requiredInsertCount == 0) {
+                encodeInteger(prefixBuffer, 0, 0x00, 8)
+                encodeInteger(prefixBuffer, 0, 0x00, 7)
+            } else {
+                val wireRIC = (requiredInsertCount % (2 * getMaxEntries())) + 1
+                encodeInteger(prefixBuffer, wireRIC, 0x00, 8)
+                if (base >= requiredInsertCount) {
+                    encodeInteger(prefixBuffer, base - requiredInsertCount, 0x00, 7)
+                } else {
+                    encodeInteger(prefixBuffer, requiredInsertCount - base - 1, 0x80, 7)
+                }
+            }
+
+            // TODO:
+            // return encoderBuffer, prefixBuffer + streamBuffer
+
+            return prefixBuffer.readByteString()
+        }
+
+        private fun getMaxEntries(): Int {
+            return maxDynamicTableCapacity / 32
+        }
+
+        private fun shouldIndex(header: Header) = true // TODO
+        private fun dynamicTableCanIndex(header: Header) = true // TODO
+
+        private fun findInStaticTable(header: Header): Int {
+            return staticTable.indexOf(header) // TODO: make more efficient
+        }
+
+        private fun findNameInStaticTable(name: ByteString): Int {
+            return staticTable.indexOfFirst { it.name == name } // TODO: make more efficient
+        }
+
+        private fun findInDynamicTable(header: Header): Int {
+            return dynamicTable.indexOf(header) // TODO: make more efficient
+        }
+
+        private fun findNameInDynamicTable(name: ByteString): Int {
+            return staticTable.indexOfFirst { it.name == name } // TODO: make more efficient
         }
 
     }
 
+}
+
+/**
+ * Encodes a prefix integer, as described in [RFC7541 section 5](https://datatracker.ietf.org/doc/html/rfc7541#section-5)
+ */
+internal fun encodeInteger(buffer: Buffer, value: Int, prefix: Int, prefixLength: Int) {
+    require(prefixLength in 1..8) {
+        "prefix length must be between 1 and 8"
+    }
+    require(prefix in 0..255) {
+        "prefix must be between 0 and 255"
+    }
+
+    val upperBoundForFirstByte = (2 shl prefixLength - 1) - 1 // == 2^N - 1
+    if (value < upperBoundForFirstByte) {
+        // value fits in single byte , including the prefix mask
+        // if prefixLength == 5, this byte will be `pppvvvvv` - first 3 bits of prefix, last bits the value itself
+        buffer.writeByte(prefix or value)
+        return
+    }
+
+    // value does not fit in prefix byte, so fill up prefix with '1'
+    buffer.writeByte(prefix or upperBoundForFirstByte)
+    var restOfValue = value - upperBoundForFirstByte
+
+    // write least significant 7 bits first, preceded by '1' bit
+    while (restOfValue > 0x80) {
+        buffer.writeByte(restOfValue % 0x80 - 0x80)
+        restOfValue /= 0x80
+    }
+    // write most significant bits, preceded by '0' bit
+    buffer.writeByte(restOfValue)
+}
+
+internal fun encodeInsert(staticNameIndex: Int, dynamicNameIndex: Int, header: Header) {
+    TODO("Not yet implemented")
+}
+
+internal fun encodeDynamicIndexReference(dynamicIndex: Int, base: Int) {
+    TODO("Not yet implemented")
+}
+
+internal fun encodeLiteral(staticNameIndex: Int, line: Header) {
+    TODO("Not yet implemented")
+}
+
+internal fun encodeDynamicLiteral(dynamicNameIndex: Int, base: Int, header: Header) {
+    TODO("Not yet implemented")
+}
+
+internal fun encodeStaticIndexReference(buffer: Buffer, staticIndex: Int) {
+    TODO("Not yet implemented")
 }
 
 enum class AcknowledgementMode {
--- src/commonTest/kotlin/de/marcreichelt/qpack/QpackWriterTest.kt
+++ src/commonTest/kotlin/de/marcreichelt/qpack/QpackWriterTest.kt
@@ -1,7 +1,9 @@
 package de.marcreichelt.qpack
 
+import okio.Buffer
 import okio.ByteString
 import okio.ByteString.Companion.decodeHex
+import okio.ByteString.Companion.toByteString
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -10,19 +12,76 @@ import kotlin.test.assertEquals
 class QpackWriterTest {
 
     @Test
+    fun writeSingleByteIntZero() {
+        val buffer = Buffer()
+        encodeInteger(buffer, value = 0, 0x00, 8)
+        assertEquals("00".hex, buffer.readByteString())
+    }
+
+    @Test
+    fun writeSingleByteInt() {
+        val buffer = Buffer()
+        encodeInteger(buffer, value = 42, 0x00, 8)
+        assertEquals("2a".hex, buffer.readByteString())
+    }
+
+    @Test
+    fun writeSingleByteWithPrefix() {
+        val buffer = Buffer()
+        encodeInteger(buffer, value = 0b00111111, prefix = 0b10000000, 7)
+        assertEquals(int2Bytes(0b10111111), buffer.readByteString())
+    }
+
+    @Test
+    fun writeTwoByteWithPrefix() {
+        val buffer = Buffer()
+        encodeInteger(buffer, value = 0xffff, 0b10000000, 7)
+        // 0xffff = 65535
+        // 2^N - 1 = 127
+        // 65535 - 127 = 65408
+        // 65408 = 0b11111111_10000000
+        // encoded in reverse order: least significant 7 bits (0000000) first, then next 7 (1111111), then most significant 7 bits (0000011)
+        // all octets start with 1, last starts with 0:     0b10000000                   0b11111111                              0b00000011
+        //                             1           1           0
+        assertEquals(int2Bytes(0xff, 0b10000000, 0b11111111, 0b00000011), buffer.readByteString())
+
+        // same as above, in hex
+        encodeInteger(buffer, value = 0xffff, 0x80, 7)
+        assertEquals("ff80ff03".hex, buffer.readByteString())
+    }
+
+    @Test
     fun writeEmptyList() {
         assertEquals("0000".hex, write(emptyList()))
     }
+//
+//    @Test
+//    fun emptyName() {
+//        val headers = listOf(Header("", "foo"))
+//        assertEquals("0000208294e7".hex, write(headers))
+//    }
+//
+//    @Test
+//    fun emptyValue() {
+//        val headers = listOf(Header("foo", ""))
+//        assertEquals("00002a94e700".hex, write(headers))
+//    }
 
     private val String.hex: ByteString
         get() = decodeHex()
 
+    private fun int2Bytes(vararg ints: Int): ByteString {
+        return ints.map { it.toByte() }.toByteArray().toByteString()
+    }
+
     private fun write(headers: List<Header>): ByteString {
         val writer = Qpack.Writer(
+            out = Buffer(),
+            encoderBuffer = Buffer(),
             maxBlockedStreams = 1,
             acknowledgementMode = AcknowledgementMode.None
         )
-        return writer.encode(QuicStreamId(1), headers)
+        return writer.encode(headers)
     }
 
 }
